492ef6a3034ecb1a0be925e06be051d24b09a0c3
stefafafan <github.le5ke@stenyan.jp>
2024-04-06 23:56:41 +0900

    set php-cs-fixer ruleset to Symfony

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,8 +6,7 @@ $finder = (new PhpCsFixer\Finder())
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PER-CS' => true,
-        '@PHP82Migration' => true,
+        '@Symfony' => true,
     ])
     ->setFinder($finder)
 ;
